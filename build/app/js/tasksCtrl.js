351c8999f08dbc09e9ecfa0dad4b39cb861563b0
mirman-mtaggart <mtaggart@mirman.org>
2016-02-04 14:38:19 -0800

    Add auth

--- build/app/js/tasksCtrl.js
+++ build/app/js/tasksCtrl.js
@@ -1,13 +1,17 @@
 angular.module("mirmindr")
 .controller("tasksCtrl",["$scope","$firebaseObject",function($scope,$firebaseObject){
+  chrome.identity.getProfileUserInfo(function(token){
+    console.log(token);
+  });
+  $scope.loginMessage = "Logging in...";
   $scope.authenticated = false;
   var ref = new Firebase("https://mirmindr.firebaseio.com");
-  ref.authWithOAuthPopup("google", function(error, authData) {
-    if (error) {
-      console.log("Login Failed!", error);
-    } else {
-      console.log("Authenticated successfully with payload:", authData);
-      $scope.authenticated = true;
-    }
-  });
+  // ref.authWithCustomToken("google", function(error, authData) {
+  //   if (error) {
+  //     console.log("Login Failed!", error);
+  //   } else {
+  //     console.log("Authenticated successfully with payload:", authData);
+  //     $scope.authenticated = true;
+  //   }
+  // });
 }]);
