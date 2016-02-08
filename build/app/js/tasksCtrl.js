9a2b07187ab80834e218a13862ddf947ec265c41
mirman-mtaggart <mtaggart@mirman.org>
2016-02-07 19:00:21 -0800

    Add login error popups

--- build/app/js/tasksCtrl.js
+++ build/app/js/tasksCtrl.js
@@ -1,21 +1,55 @@
 angular.module("mirmindr")
-.controller("tasksCtrl",["$scope","$firebaseObject",function($scope,$firebaseObject){
+.controller("tasksCtrl",function($scope,$mdToast,$mdDialog,$firebaseObject){
   var ref = new Firebase("https://mirmindr.firebaseio.com");
   $scope.loginMessage = "Logging in...";
   $scope.authenticated = false;
-  $scope.user = {};
-  // ref.authWithOAuthPopup("google", function(error, authData) {
-  //   if (error) {
-  //     console.log("Login Failed!", error);
-  //   } else {
-  //     console.log("Authenticated successfully with payload:", authData);
-  //     $scope.authenticated = true;
-  //   }
-  // });
+  $scope.user = {
+    email: "",
+    password: ""
+  };
+  chrome.identity.getProfileUserInfo(function(data){
+    if(data.email) {
+      console.log("Email found");
+      $scope.user.email = data.email;
+      $scope.$apply();
+    }
+  });
+
+  $scope.createUser = function(user) {
+    ref.createUser(user,function(error, userData){
+      if(error) {
+        console.log(error);
+      } else {
+        console.log(userData);
+        $scope.authenticated = true;
+        $scope.$apply();
+      }
+    })
+  };
+
+  $scope.showActionToast = function(msg) {
+    var toast = $mdToast.simple()
+          .textContent(msg)
+          .action('OK')
+          .highlightAction(false)
+          .position("top");
+    $mdToast.show(toast);
+  };
 
   $scope.login = function(form) {
     if (form.$valid) {
-      console.log($scope.user.email);
+      ref.authWithPassword({
+        email: $scope.user.email,
+        password: $scope.user.password
+      }, function(error, authData) {
+        if(error) {
+          $scope.showActionToast(error.code);
+        } else {
+          console.log("Authenticated");
+          $scope.authenticated = true;
+          $scope.$apply();
+        }
+      });
     }
   }
-}]);
+});
