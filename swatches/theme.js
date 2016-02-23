377c25c1f9b3070d493857beb38dedb689c4d77d
mirman-kyoppenheim2016 <kyoppenheim2016@mirmanmustangs.org>
2016-02-23 13:15:12 -0800

    Theme template written, partially complete

---
+++ swatches/theme.js
@@ -0,0 +1,10 @@
+//Template, will be subject to revision
+
+angular.module('mirmindr', ['ngMaterial'])
+.config(function($mdThemingProvider) {
+  $mdThemingProvider.theme('mirmindrTheme')
+  .primaryPalette('')
+  .accentPalette('')
+  .warnPalette('')
+  .backgroundPalette('');
+});
