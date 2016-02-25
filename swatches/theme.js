0561b315a4bb0f38a5b1c6d943aa945e52ea234a
mirman-kyoppenheim2016 <kyoppenheim2016@mirmanmustangs.org>
2016-02-25 14:55:17 -0800

    Revision 1 theme.js

--- swatches/theme.js
+++ swatches/theme.js
@@ -1,8 +1,9 @@
 //Template, will be subject to revision
 
-angular.module('mirmindr', ['ngMaterial'])
+angular.module('mirmindr')
 .config(function($mdThemingProvider) {
   $mdThemingProvider.theme('mirmindrTheme')
+  var mirmindrPrimary = $mdThemingProvider.extendPalette('')
   .primaryPalette(#f0f4f5)
   .accentPalette(#173345)
   .warnPalette(#f5715f)
