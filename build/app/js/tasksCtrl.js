1c43d9f381f3b44e2bd64a09f7a1554b2cccae1e
mirman-wuhls2017 <wauhls2017@mirmanmustangs.org>
2016-03-10 14:43:46 -0800

    Revert "Beautified tasksCtrl.js, fixed broken condition statement"
    This reverts commit 8618b15e0c4340e577fb8b98142b53f92db9c211.

--- build/app/js/tasksCtrl.js
+++ build/app/js/tasksCtrl.js
@@ -1,124 +1,128 @@
 angular.module("mirmindr")
-  .controller("tasksCtrl", function($scope, $mdToast, $mdDialog, $firebaseArray) {
-    var ref = new Firebase("https://mirmindr.firebaseio.com");
-    var authData = ref.getAuth();
+.controller("tasksCtrl",function($scope,$mdToast,$mdDialog,$firebaseArray){
+  var ref = new Firebase("https://mirmindr.firebaseio.com");
+  var authData = ref.getAuth();
+  function setUserRef(uid){
+    $scope.userRef = ref.child("users").child(uid);
+    $scope.authenticated = true;
+    $scope.tasks = $firebaseArray($scope.userRef.child("tasks"));
+    $scope.subjects = $firebaseArray($scope.userRef.child("subjects"));
+  }
+  if (authData) {
+    console.log("Authenticated");
+    setUserRef(authData.uid);
+  }
+  $scope.user = {
+    email: "",
+    password: ""
+  };
 
-    function setUserRef(uid) {
-      $scope.userRef = ref.child("users").child(uid);
-      $scope.authenticated = true;
-      $scope.tasks = $firebaseArray($scope.userRef.child("tasks"));
-      $scope.subjects = $firebaseArray($scope.userRef.child("subjects"));
+  $scope.toggleSubjects = function() {
+    $scope.editingSubjects = $scope.editingSubjects ? false : true;
+    // If editingSubjects is true, make it false. Or vice versa
+  };
+
+  $scope.toggleAddingTask = function() {
+    $scope.addingTask = $scope.addingTask ? false : true;
+    // If addingTask is true, make it false. Or vice versa
+  };
+  $scope.toggleEditingTask = function(task) {
+    if($scope.editingTask==true)
+    {
+
+ $scope.editingTask=false;
+    $scope.newTask={};
     }
-    if (authData) {
-      console.log("Authenticated");
-      setUserRef(authData.uid);
+    else {
+      $scope.newTask=task;
+      $scope.newTask.dueDate= new Date($scope.newTask.dueDate);
+      $scope.editingTask=true;
+
     }
-    $scope.user = {
-      email: "",
-      password: ""
-    };
 
-    $scope.toggleSubjects = function() {
-      $scope.editingSubjects = $scope.editingSubjects ? false : true;
-      // If editingSubjects is true, make it false. Or vice versa
-    };
+    // If editingTask is true, make it false. Or vice versa
+  };
+  $scope.deleteTask = function(task) {
+    $scope.tasks.$remove(task);
+    // Remove task from $scope.tasks
+  };
+  $scope.toggleDone = function(task) {
+    // Mark task as done
+    task.done = task.done ? false: true;
 
-    $scope.toggleAddingTask = function() {
-      $scope.addingTask = $scope.addingTask ? false : true;
-      // If addingTask is true, make it false. Or vice versa
-    };
-    $scope.toggleEditingTask = function(task) {
-      if ($scope.editingTask === true) {
-        $scope.editingTask = false;
-        $scope.newTask = {};
-      } else {
-        $scope.newTask = task;
-        $scope.newTask.dueDate = new Date($scope.newTask.dueDate);
-        $scope.editingTask = true;
+    // Alert with a toast
+    $scope.showActionToast("'" + task.name + "' has been marked as done");
 
-      }
+    // Save the tasks array
+    $scope.tasks.$save(task);
+  };
 
-      // If editingTask is true, make it false. Or vice versa
-    };
-    $scope.deleteTask = function(task) {
-      $scope.tasks.$remove(task);
-      // Remove task from $scope.tasks
-    };
-    $scope.toggleDone = function(task) {
-      // Mark task as done
-      task.done = task.done ? false : true;
+  $scope.isOverdue = function(task) {
+    // Return true if the task's dueDate is older than now.
+  };
 
-      // Alert with a toast
-      $scope.showActionToast("'" + task.name + "' has been marked as done");
+  $scope.newTask = {};
+  chrome.identity.getProfileUserInfo(function(data){
+    if(data.email) {
+      console.log("Email found");
+      $scope.user.email = data.email;
+      $scope.$apply();
+    }
+  });
 
-      // Save the tasks array
-      $scope.tasks.$save(task);
-    };
+  $scope.showActionToast = function(msg) {
+    var toast = $mdToast.simple()
+          .textContent(msg)
+          .action('OK')
+          .highlightAction(false)
+          .position("top");
+    $mdToast.show(toast);
+  };
 
-    $scope.isOverdue = function(task) {
-      // Return true if the task's dueDate is older than now.
-    };
-
-    $scope.newTask = {};
-    chrome.identity.getProfileUserInfo(function(data) {
-      if (data.email) {
-        console.log("Email found");
-        $scope.user.email = data.email;
-        $scope.$apply();
-      }
-    });
-
-    $scope.showActionToast = function(msg) {
-      var toast = $mdToast.simple()
-        .textContent(msg)
-        .action('OK')
-        .highlightAction(false)
-        .position("top");
-      $mdToast.show(toast);
-    };
-
-    $scope.login = function(form) {
-      if (form.$valid) {
-        ref.authWithPassword({
-          email: $scope.user.email,
-          password: $scope.user.password
-        }, function(error, authData) {
-          if (error) {
-            $scope.showActionToast(error.toString());
-          } else {
-            setUserRef(authData.uid);
-            $scope.$apply();
-          }
-        });
-      }
-    };
-
-    $scope.logout = function() {
-      ref.unauth();
-      $scope.authenticated = false;
-    };
-
-
-    $scope.addTask = function(form) {
-      if (form.$valid) {
-
-
-        $scope.newTask.dueDate = $scope.newTask.dueDate.getTime();
-        $scope.newTask.done = $scope.newTask.done || false;
-        if ($scope.editingTask) {
-
-          $scope.tasks.$save($scope.newTask);
-          $scope.editingTask = false;
+  $scope.login = function(form) {
+    if (form.$valid) {
+      ref.authWithPassword({
+        email: $scope.user.email,
+        password: $scope.user.password
+      }, function(error, authData) {
+        if(error) {
+          $scope.showActionToast(error.toString());
         } else {
-          //  console.log($scope.newTask);
+          setUserRef(authData.uid);
+          $scope.$apply();
+        }
+      });
+    }
+  };
+
+  $scope.logout = function() {
+    ref.unauth();
+    $scope.authenticated = false;
+  };
+
+
+  $scope.addTask = function(form) {
+    if(form.$valid) {
+
+
+          $scope.newTask.dueDate = $scope.newTask.dueDate.getTime();
+          $scope.newTask.done=$scope.newTask.done || false;
+      if($scope.editingTask)
+      {
+
+        $scope.tasks.$save($scope.newTask);
+        $scope.editingTask=false;
+      }
+      else{
+        //  console.log($scope.newTask);
 
           $scope.newTask.done = false;
           $scope.tasks.$add($scope.newTask);
           $scope.newTask = {};
-          $scope.addingTask = false;
-        }
-      } else {
-        $scope.showActionToast("Missing something?");
+            $scope.addingTask = false;
       }
-    };
-  });
+    } else {
+      $scope.showActionToast("Missing something?");
+    }
+  };
+});
