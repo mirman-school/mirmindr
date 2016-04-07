2ad606d542479ccd09f14ad807137a6a8bfd8f50
mirman-mtaggart <mtaggart@mirman.org>
2016-04-07 14:27:59 -0700

    Overwrite old controller

--- build/app/js/subjectsCtrl.js
+++ build/app/js/subjectsCtrl.js
@@ -11,51 +11,8 @@ angular.module("mirmindr")
     }
   };
 
-
-  if($scope.editingSubjects)
-{
-$scope.subs.$save($scope.newSub);
-$scope.editingSubjects=false;
-}
-else{
-//  console.log($scope.newTask);
-
-  $scope.newSub.done = false;
-  $scope.subjects.$add($scope.newTask);
-  $scope.newSub = {};
-}
-
-
-//   $scope.deleteSubject = function(sub) {
-//     var deleteConfirm = confirm("Are you sure you want to delete the subject? This will delete all tasks under this subject.");
-//     if (deleteConfirm) {
-//       $scope.subjects.$remove(sub);
-//     }
-//   };
-// });
-  //   $scope.deleteSubject = function(sub) {
-  //     var deleteConfirm = confirm("Are you sure you want to delete the subject? This will delete all tasks under this subject.");
-  //     if (deleteConfirm) {
-  //       $scope.subjects.$remove(sub);
-  //     }
-  //   };
-  // });
-$scope.editSubject = function(sub){
-  if($scope.editingSubject===true) {
-  $scope.editingSubject=false;
-  $scope.newSub={};
-  } else {
-    $scope.newSub.name=sub.name;
-    $scope.newSub.period=sub.period;
-    $scope.oldSub=sub;
-    $scope.editingSubject=true;
-    console.log($scope.sub);
-
-
-  }
   $scope.setCurrentSubject = function(sub) {
     $scope.currentSubject = sub;
-
   };
 
   $scope.deleteSubject = function(sub) {
